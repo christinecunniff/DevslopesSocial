559ebea2f96b34585cc2869bdf04c143a2b2f002
Christine Cunniff <christinecunniff@gmail.com>
2017-01-26 16:34:13 -0500

    Completed Facebook and Firebase authorization.

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -1,4 +1,7 @@
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -12,6 +15,31 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("CHRISTINE: Unable to authenticate with Facebook.")
+            } else if result?.isCancelled == true {
+                print("CHRISTINE: User cancelled facebook authentication")
+            } else {
+                print("CHRISTINE: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+    }
+    
+    func firebaseAuthenticate(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("CHRISTINE: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("CHRISTINE: Successfully authenticated with Firebase")
+            }
+        })
+    }
+    
 }
 
