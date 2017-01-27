a5226adc9016340c0dd533727b8d2d15c852fd91
Christine Cunniff <christinecunniff@gmail.com>
2017-01-27 09:18:23 -0500

    Completed email auth via Firebase.

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -5,6 +5,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,6 +43,25 @@ class SignInVC: UIViewController {
             }
         })
     }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("CHRISTINE: Email user authenticated with Firebase")
+                } else {
+                  FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                    if error != nil {
+                        print("CHRISTINE: Unable to uthenticate with Firebase using email")
+                    } else {
+                        print("CHRISTINE: Successfully authenticated with Firebase")
+                    }
+                  })
+                }
+            })
+        }
+    }
+    
     
 }
 
